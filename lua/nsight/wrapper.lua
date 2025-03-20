828ce53726334bd37fdfa2d1a88c10bdaface4d3
Jorgedavyd <jorged.encyso@gmail.com>
2025-03-20 16:29:45 -0300

    wrapper

--- lua/nsight/wrapper.lua
+++ lua/nsight/wrapper.lua
@@ -10,64 +10,57 @@ local config = require("nsight.config").opts
 local W = {}
 
 function W:new(basecmd)
-    setmetatable(W, self)
-    self.basecmd = basecmd
-    return self
+    local instance = {
+        basecmd = basecmd
+    }
+    return setmetatable(instance, { __index = self })
 end
 
 function string.split(inputstr, sep)
-  if sep == nil then
-    sep = "%s"
-  end
-  local t = {}
-  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-    table.insert(t, str)
-  end
-  return t
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+    return t
 end
 
-local function parse_args(k, args)
-    if type(string) == "table" then
+local function parse_args(cmd, args)
+    if type(args) == "string" then
         args = string.split(args, " ")
     end
-    local cmd = {k}
-    vim.list_extend(cmd, args)
-    return cmd
+    local result = {cmd}
+    vim.list_extend(result, args)
+    return result
 end
 
-setmetatable(W,{
-    __index = function (_, k)
-        if k == "ui" then
-            utils.LogNotImplemented("UI not implemented for this command")
-            return
-        elseif k == "__call" then
-            return function(args)
-                local cmd  = parse_args(k, args)
-                vim.system(cmd, {text = true}, function (out)
-                    if out.code == 0 then
-                        utils.LogInfo(string.format("%s ran succesfully.", k))
-                        vim.notify(out.stdout, vim.log.levels.INFO)
-                        return
-                    else
-                        utils.LogInfo(string.format("%s failed with error code %s.", k, out.code))
-                        vim.notify(out.stderr, vim.log.levels.ERROR)
-                        return
-                    end
-                end)
-            end
+function W:__call(args)
+    local cmd = parse_args(self.basecmd, args or {})
+    vim.system(cmd, {text = true}, function(out)
+        if out.code == 0 then
+            utils.LogInfo(string.format("%s ran successfully.", self.basecmd))
+            vim.notify(out.stdout, vim.log.levels.INFO)
         else
-            utils.LogError("Not a valid field")
-            return
+            utils.LogError(string.format("%s failed with error code %s.", self.basecmd, out.code))
+            vim.notify(out.stderr, vim.log.levels.ERROR)
         end
-    end
-})
+    end)
+end
 
+function W:ui(args)
+    utils.LogNotImplemented("UI not implemented for this command")
+end
 
 ---@class NsightWrapper
 ---@field tools [string]
 ---@field setup fun():nil
+---@field nvcc NsightWrapper
+---@field ncu NsightWrapper
+---@field nsys NsightWrapper
+---@field nvvp NsightWrapper
 local M = {}
-
 M.tools = {"nvcc", "ncu", "nvvp", "nsys"}
 
 function M.setup()
@@ -75,14 +68,23 @@ function M.setup()
     if opts ~= nil then
         vim.list_extend(M.tools, opts)
     end
-    M = vim.tbl_extend("keep", M, vim.tbl_map(function (value) return W:new(value) end, M.tools))
-    function M.ncu.ui(args)
-        local wrap = W:new("ncu-ui")
-        return wrap(args)
+
+    for _, tool in ipairs(M.tools) do
+        M[tool] = W:new(tool)
     end
-    function M.nsys.ui(args)
-        local wrap = W:new("nsys-ui")
-        return wrap(args)
+
+    if M.ncu then
+        function M.ncu.ui(args)
+            local wrap = W:new("ncu-ui")
+            return wrap(args)
+        end
+    end
+
+    if M.nsys then
+        function M.nsys.ui(args)
+            local wrap = W:new("nsys-ui")
+            return wrap(args)
+        end
     end
 end
 
