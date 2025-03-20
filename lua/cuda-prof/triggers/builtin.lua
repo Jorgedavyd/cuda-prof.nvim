ed97005654c7f8421cce1d8acadf7d536425673b
Jorgedavyd <jorged.encyso@gmail.com>
2025-03-20 10:15:56 -0300

    default

--- lua/cuda-prof/triggers/builtin.lua
+++ lua/cuda-prof/triggers/builtin.lua
@@ -1,3 +1,32 @@
 local wrapper = require("cuda-prof.wrapper")
-local M = {}
-return M
+
+---@class CudaProfBuiltinTriggers
+---@field private wrapper_ fun():nil
+---@field __call fun():nil
+
+---@type CudaProfBuiltinTriggers
+local nsys_trigger = {
+    wrapper_ = wrapper.nsys.__call,
+    __call = function()
+    end
+}
+
+---@type CudaProfBuiltinTriggers
+local nvcc_trigger = {
+    wrapper_ = wrapper.nvcc.__call,
+    __call = function()
+    end
+}
+
+---@type CudaProfBuiltinTriggers
+local ncu_trigger = {
+    wrapper_ = wrapper.ncu.__call,
+    __call = function()
+        self.wrapper_()
+    end
+}
+
+return {
+    nvcc_trigger = nvcc_trigger,
+    ncu_trigger = ncu_trigger,
+}
