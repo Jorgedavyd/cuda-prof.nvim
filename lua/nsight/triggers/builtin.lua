fa44147ef94f966ba5be244a7079886218d36e97
Jorgedavyd <jorged.encyso@gmail.com>
2025-03-21 19:54:53 -0300

    nice metaprogramming scheme for triggers

--- lua/nsight/triggers/builtin.lua
+++ lua/nsight/triggers/builtin.lua
@@ -6,15 +6,25 @@ local sequences = require("nsight.triggers.sequences")
 
 ---@type NsightBuiltinTrigger
 return setmetatable({}, {
-    __index = function (_, k)
+    __index = function(_, k)
         local methods = vim.split(k, "_")
-        if methods ~= nil and #methods ~=0 then
-            return function (opts)
-                return trigger:new(sequences:new(vim.tbl_map(function (value)
-                    local wrap = require("nsight.wrapper")[value]
-                    return wrap and wrap(opts) or utils.LogError("Not a valid field")
+        if #methods > 0 then
+            return function(opts)
+                return trigger:new(sequences:new(vim.tbl_map(function(value)
+                    local sampled_method = vim.split(value, "-")
+                    local wrap = require("nsight.wrapper")
+                    for _, part in ipairs(sampled_method) do
+                        if wrap and wrap[part] then
+                            wrap = wrap[part]
+                        else
+                            utils.LogError("Invalid method: " .. value)
+                            return nil
+                        end
+                    end
+                    return wrap and wrap(opts)
                 end, methods)))
             end
         end
     end
 })
+
