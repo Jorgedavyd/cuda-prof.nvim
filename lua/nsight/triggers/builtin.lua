e115c1f763ec4842abd0042f9fcd1a979e702279
Jorgedavyd <jorged.encyso@gmail.com>
2025-03-21 19:40:19 -0300

    setting up builtin triggers

--- lua/nsight/triggers/builtin.lua
+++ lua/nsight/triggers/builtin.lua
@@ -1,18 +1,20 @@
-local wrapper = require("nsight.wrapper")
 local trigger = require("nsight.triggers.init")
+local utils = require("nsight.utils")
 local sequences = require("nsight.triggers.sequences")
 
----@class NsightBuiltinTrigger
----@field nvcc_nsys NsightTriggers
----@field nvcc_ncu NsightTriggers
----@field nvcc_nvvp NsightTriggers
----@field nvcc NsightTriggers
-local M = {}
+---@alias NsightBuiltinTrigger fun(opts: table<string, string>): NsightTriggers
 
-M.nvcc_nsys = trigger:new(sequences:new({
-    function ()
-        wrapper.nvcc("")
+---@type NsightBuiltinTrigger
+return setmetatable({}, {
+    __index = function (_, k)
+        local methods = vim.split(k, "_")
+        if methods ~= nil and #methods ~=0 then
+            return function (opts)
+                return trigger:new(sequences:new(vim.tbl_map(function (value)
+                    local wrap = require("nsight.wrapper")[value]
+                    return wrap and wrap(opts) or utils.LogError("Not a valid field")
+                end, methods)))
+            end
+        end
     end
-}))
-
-return M
+})
